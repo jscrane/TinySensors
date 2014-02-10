283ae1c2b737b2010abdc70cf57aa57b12234ac2
Stephen Crane <jscrane@gmail.com>
2014-02-10 09:16:22 +0000

    db

---
+++ sensors.sql
@@ -0,0 +1,105 @@
+-- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (armv7l)
+--
+-- Host: localhost    Database: sensors
+-- ------------------------------------------------------
+-- Server version	5.5.35-0+wheezy1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `device_types`
+--
+
+DROP TABLE IF EXISTS `device_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `device_types` (
+  `id` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `nodes`
+--
+
+DROP TABLE IF EXISTS `nodes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nodes` (
+  `id` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `location` varchar(255) DEFAULT NULL,
+  `type` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`),
+  CONSTRAINT `nodes_ibfk_1` FOREIGN KEY (`type`) REFERENCES `device_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sensordata`
+--
+
+DROP TABLE IF EXISTS `sensordata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sensordata` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `node_id` smallint(5) unsigned NOT NULL,
+  `node_ms` int(10) unsigned NOT NULL,
+  `light` tinyint(3) unsigned NOT NULL,
+  `humidity` float(4,1) DEFAULT NULL,
+  `temperature` float(4,1) DEFAULT NULL,
+  `battery` float(3,2) NOT NULL,
+  `th_status` tinyint(3) unsigned NOT NULL,
+  `msg_id` int(10) unsigned NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `node_id` (`node_id`),
+  CONSTRAINT `sensordata_ibfk_1` FOREIGN KEY (`node_id`) REFERENCES `nodes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=426178 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `weather`
+--
+
+DROP TABLE IF EXISTS `weather`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `weather` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last_updated` varchar(24) NOT NULL,
+  `temperature` tinyint(4) NOT NULL,
+  `humidity` tinyint(3) unsigned NOT NULL,
+  `visibility` float(4,1) NOT NULL,
+  `pressure` float(6,2) NOT NULL,
+  `feels_like` tinyint(4) NOT NULL,
+  `direction` smallint(6) NOT NULL,
+  `speed` tinyint(3) unsigned NOT NULL,
+  `gust` tinyint(3) unsigned NOT NULL,
+  `icon` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1041 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-02-10  9:16:01
