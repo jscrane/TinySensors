a25da23eac00c82ba9c34c6127c6f815abd18dca
Stephen Crane <jscrane@gmail.com>
2015-02-16 11:00:43 +0000

    renumbering sensors

---
+++ scripts/sensors.pm
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+
+%sensors = (
+	"26.01CDCF000000" => { light => 1, short => "wind", name => "bedroom window", colour => "#FF00FF", id => 20 },
+	"26.E4D8CF000000" => { light => 1, short => "hall", name => "hallway", colour => "#00FFFF", id => 21 },
+	"28.BD7645020000" => { light => 0, short => "bedr", name => "bedroom", colour => "#00FF00", id => 22 },
+	"26.40D7CF000000" => { light => 0, short => "spar", name => "spare room", colour => "#7F7F7F", id => 23 },
+	"28.93189C020000" => { light => 0, short => "livi", name => "living room", colour => "#FF0000", id => 24 },
+	"28.06BB45020000" => { light => 0, short => "gfch", name => "boiler", colour => "#3F3F3F", id => 25 },
+	"26.BAE3CF000000" => { light => 0, short => "hotp", name => "hot press", colour => "#0000FF", id => 26 },
+);
