fb3ad904f5d0b5f7abe212db561173a621f5a32c
Stephen Crane <jscrane@gmail.com>
2015-03-20 13:16:48 +0000

    more restiness

--- scripts/graphs.py
+++ scripts/graphs.py
@@ -7,27 +7,38 @@ render = web.template.render('templates/')
 
 urls = (
 	# e.g., '/battery/' or '/battery/1' or '/b/1'
-	'/(.*)/(.*)', 'sensor'
+	'/(.*)/(.*)/(.*)', 'sensor'
 )
 
-f = { 'a': 'all', 't': 'temperature', 'b': 'battery', 'h': 'humidity' }
+f = { 't': 'temperature', 'b': 'battery', 'h': 'humidity', 'l': 'light' }
 
 db = sensordb.connect("/home/pi/TinySensors/auth")
 sensors = sensordb.sensors(db)
 
-i = {}
+ids = {}
 for id,s in sensors.items():
-	i[s['short']] = id
+	ids[s['short']] = id
+
+p = { 'd': 'day', 'm': 'month', 'y': 'year' }
 
 class sensor:
-	def GET(self, feature, sensor):
-		if feature in f:
-			feature = f[feature]
-		if sensor == "":
-			sensor = "all"
-		elif sensor in i:
-			sensor = i[sensor]
-		return render.sensor(feature, sensor)
+	def GET(self, node, feature, period):
+		feats = [feature]
+		if feature == "":
+			feats = f.values()
+		elif feature in f:
+			feats = [f[feature]]
+		if node == "":
+			node = "all"
+		elif node in ids:
+			node = ids[node]
+		if period == "":
+			period = ['day', 'month', 'year']
+		elif period in p:
+			period = [p[period]]
+		else:
+			period = [period]
+		return render.sensor(node, feats, period)
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
