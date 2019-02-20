b4bfc1e375dbbf1296e07ea41e122ea3947e9a9f
Stephen Crane <jscrane@gmail.com>
2019-02-20 07:45:55 +0000

    radio init; flash strings

--- sensortest.ino
+++ sensortest.ino
@@ -1,3 +1,5 @@
+#include <SPI.h>
+#include <RF24.h>
 #include <DHT.h>
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
@@ -8,7 +10,9 @@ const int RX_PIN = 1, TX_PIN = 0;
 */
 const uint8_t DHT_PIN = 2;
 const uint8_t RX_PIN = 9, TX_PIN = 10;
+const uint8_t CE_PIN = 8, CS_PIN = 7;
 
+RF24 radio(CE_PIN, CS_PIN);
 DHT dht;
 SoftwareSerial serial(RX_PIN, TX_PIN);
 
@@ -17,13 +21,19 @@ void setup(void)
 	pinMode(RX_PIN, INPUT);
 	pinMode(TX_PIN, OUTPUT);
 
-	serial.begin(9600);
-	serial.println("Sensor Test. Enter node-id and hit return.");
-	serial.println("Status\tHumidity\tTemperature\tLight\tBattery\tID");
-
 	pinMode(A1, INPUT_PULLUP);
 
 	dht.setup(DHT_PIN);
+
+	SPI.begin();
+	radio.begin();
+
+	serial.begin(9600);
+	serial.print(F("retries: "));
+	serial.println(radio.getRetries(), 16);
+
+	serial.println(F("Sensor Test. Enter node-id and hit return."));
+	serial.println(F("Status\tHumidity\tTemperature\tLight\tBattery\tID"));
 }
 
 void loop(void)
@@ -31,16 +41,16 @@ void loop(void)
 	uint8_t light = 255 - analogRead(A1) / 4;
 
 	serial.print(dht.getStatus());
-	serial.print("\t");
+	serial.print(F("\t"));
 	serial.print(dht.getHumidity());
-	serial.print("\t\t");
+	serial.print(F("\t\t"));
 	serial.print(dht.getTemperature());
-	serial.print("\t\t");
+	serial.print(F("\t\t"));
 	serial.print((int)light);
-	serial.print("\t");
+	serial.print(F("\t"));
 	serial.print(analogRead(A0));
-	serial.print("\t");
-	serial.println(EEPROM.read(0), 8);
+	serial.print(F("\t"));
+	serial.println(EEPROM.read(0));
 
 	uint8_t id = 0;
 	while (serial.available()) {
