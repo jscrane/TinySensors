021e9cda8f3baa3b3f0c558c52661d8d4aebee69
Stephen Crane <jscrane@gmail.com>
2019-02-21 09:00:31 +0000

    code cleanup

--- tinysensor/README.md
+++ tinysensor/README.md
@@ -1,4 +1,13 @@
+### Using uC-Makefile and avrdude
+```
+$ make -I ~/src/uC-Makefile write-fuses write-flash
+$ avrdude -v -p t84 -P /dev/ttyUSB0 -b 19200 -c avrisp -U eeprom:w:1.eep
+```
+### Avrdude only
+```
 $ avrdude -v -p t84 -P /dev/ttyUSB0 -b 19200 -c avrisp -U flash:w:fw.bin -U eeprom:w:1.eep -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xfe:m
+```
 
-Note that the sketch doesn't work with the current attiny core. Probably
-the pin assignments have changed. Need to check against the circuit.
+This sketch now requires the damellis core for
+[attiny](https://github.com/damellis/attiny) and my
+[SPI](https://github.com/jscrane/SPI) library.
--- tinysensor/tinysensor.ino
+++ tinysensor/tinysensor.ino
@@ -7,55 +7,62 @@
 #include "tinysensor.h"
 #include "sleepy.h"
 
-RF24 radio(2, 3);
+const uint8_t CE_PIN = 8;
+const uint8_t CS_PIN = 7;
+const uint8_t DHT_PIN = 2;
+
+RF24 radio(CE_PIN, CS_PIN);
 RF24Network network(radio);
 DHT dht;
 
+const uint8_t channel = 90;
 const uint16_t master_node = 0;
 uint16_t this_node;
+const uint8_t retry_count = 5;		// 250+5*15*250 = 19mS
+const uint8_t retry_delay = 15;		// 15*250uS
 
-const unsigned long interval = 60000; //ms
+const uint32_t interval = 60000;
+const uint32_t min_interval = 15000;
 
 void setup(void)
 {
-  pinMode(9, INPUT);
-  digitalWrite(9, HIGH);   // enable pullup
+	pinMode(A1, INPUT_PULLUP);
 
-  dht.setup(8);
-  
-  SPI.begin();
-  radio.begin();
-  
-  radio.enableDynamicPayloads();
-  radio.setAutoAck(true);
+	dht.setup(DHT_PIN);
 
-  this_node = EEPROM.read(0);
-  network.begin(90, this_node);
+	SPI.begin();
+	radio.begin();
+
+	radio.enableDynamicPayloads();
+	radio.setRetries(retry_delay, retry_count);
+
+	this_node = EEPROM.read(0);
+	network.begin(channel, this_node);
 }
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 void loop(void)
 {
-  radio.powerUp();
-  network.update();  
+	radio.powerUp();
+	network.update();
 
-  uint8_t light = 255 - analogRead(A1) / 4;
-  uint16_t battery = analogRead(A0);
+	uint8_t light = 255 - analogRead(A1) / 4;
+	uint16_t battery = analogRead(A0);
 
-  sensor_payload_t payload = { millis(), light, dht.getStatus(), dht.getHumidity(), dht.getTemperature(), battery };
-  RF24NetworkHeader header(master_node, sensor_type_id);
-  bool ok = network.write(header, &payload, sizeof(payload));
+	sensor_payload_t payload = { millis(), light, dht.getStatus(), dht.getHumidity(), dht.getTemperature(), battery };
+	RF24NetworkHeader header(master_node, sensor_type_id);
+	bool ok = network.write(header, &payload, sizeof(payload));
 
-  radio.powerDown();
+	radio.powerDown();
 
-  int32_t l = light;
-  int32_t s = 1000 * (255 - l) / 3;
-  if (s < 15000)
-    s = 15000;
-  do {
-    int32_t i = min(s, interval);  
-    Sleepy::loseSomeTime(i);
-    s -= i;
-  } while (s > 0);
+	uint32_t l = light;
+	uint32_t s = 1000 * (255 - l) / 3;
+	if (s < min_interval)
+		s = min_interval;
+	do {
+		uint32_t i = min(s, interval);
+		Sleepy::loseSomeTime(i);
+		s -= i;
+	} while (s > 0);
 }
