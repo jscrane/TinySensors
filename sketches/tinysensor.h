bbd48c45e4db21c3c79a18b2051907304e90de5e
Stephen Crane <jscrane@gmail.com>
2020-05-16 13:43:30 +0100

    change channel to 110

--- sketches/tinysensor.h
+++ sketches/tinysensor.h
@@ -1,7 +1,7 @@
 #ifndef TINYSENSOR_H
 #define TINYSENSOR_H
 
-const uint8_t channel = 108;
+const uint8_t channel = 110;
 const rf24_datarate_e data_rate = RF24_250KBPS;
 const rf24_pa_dbm_e power = RF24_PA_MAX;
 const rf24_crclength_e crc_len = RF24_CRC_8;
