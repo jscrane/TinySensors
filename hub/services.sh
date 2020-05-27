08d9233d82f44e90447b383d6492c661f6d0dbc2
Stephen Crane <jscrane@gmail.com>
2020-05-27 18:31:04 +0100

    cleanup

--- hub/services.sh
+++ hub/services.sh
@@ -5,9 +5,5 @@ SEN=/home/pi/TinySensors/hub
 [ -x $SEN/mux ] && $SEN/mux localhost:5555 sheeva:5555 < $SEN/nodes.txt
 [ -x $SEN/lcd ] && $SEN/lcd -l localhost
 [ -x $SEN/status ] && $SEN/status
-#[ -x $SEN/mqtt ] && $SEN/mqtt -q iot:1883 -m localhost:5678
-[ -x $SEN/mqtt ] && $SEN/mqtt -q mqtt.opensensors.io -m localhost:5678 \
-                    -r /users/jscrane/stat -u jscrane -p lRWhtqio -c 5693
+[ -x $SEN/mqtt ] && $SEN/mqtt -q mqtt.opensensors.io -m localhost:5678 -r /users/jscrane/stat -u jscrane -p lRWhtqio -c 5693
 [ -x $SEN/mqtt ] && $SEN/mqtt -z -q localhost:1883 -m localhost:5678 -r domoticz/in
-#[ -x $SEN/mysql ] && $SEN/mysql
-#[ -x $SEN/rrd ] && $SEN/rrd
