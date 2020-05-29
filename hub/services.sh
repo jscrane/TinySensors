a497928c76c7700c4bf793ba1dd136724af48332
Stephen Crane <jscrane@gmail.com>
2020-05-29 12:27:09 +0100

    install to /usr/local

--- hub/services.sh
+++ hub/services.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SEN=/home/pi/TinySensors/hub
+SEN=/usr/local/bin
 [ -x $SEN/sensorhub ] && $SEN/sensorhub
-[ -x $SEN/mux ] && $SEN/mux localhost:5555 sheeva:5555 < $SEN/nodes.txt
+[ -x $SEN/mux ] && $SEN/mux localhost:5555 sheeva:5555 < /usr/local/etc/nodes.txt
 [ -x $SEN/lcd ] && $SEN/lcd -l localhost
 [ -x $SEN/status ] && $SEN/status
 [ -x $SEN/mqtt ] && $SEN/mqtt -q mqtt.opensensors.io -m localhost:5678 -r /users/jscrane/stat -u jscrane -p lRWhtqio -c 5693
