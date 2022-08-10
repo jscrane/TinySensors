462a333b0db829fe9b4876e71bab558d82f09827
steve <jscrane@gmail.com>
2022-08-10 13:28:26 +0100

    add toggles to enable/disable wireless/wired sensor display for lcd

--- hub/services.sh
+++ hub/services.sh
@@ -6,7 +6,7 @@ MQTT=localhost:1883
 
 [ -x $SEN/sensorhub ] && $SEN/sensorhub
 [ -x $SEN/mux ] && $SEN/mux localhost:5555 < /usr/local/etc/nodes.txt
-[ -x $SEN/lcd ] && $SEN/lcd -l localhost -m $MUX -t 1200
+[ -x $SEN/lcd ] && $SEN/lcd -l localhost -m $MUX -t 1200 -w
 [ -x $SEN/status ] && $SEN/status -m $MUX -t 600
 [ -x $SEN/mqtt ] && $SEN/mqtt -c sensors-domoticz -z -q $MQTT -m $MUX -r domoticz/in
 [ -x $SEN/mqtt ] && $SEN/mqtt -c sensors-json -j -q $MQTT -m $MUX
